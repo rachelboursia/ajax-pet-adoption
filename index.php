9e7e4881fb79843d9b8f608939efbca6ecad1e00
rachelboursia <>
2022-02-15 19:29:26 +0000

    updates index.php

--- index.php
+++ index.php
@@ -44,16 +44,39 @@
 <script>
     $("document").ready(function(){
         
+       //hide likes and eats
+        $('#pet_likes').hide();
+         $('#pet_eats').hide();
+
+        //click feels and shows likes
+         $('#pet_feels').click(function(e){
+           $('#pet_likes').slideDown(200);
+         });
+
+        //click likes and shows eats
+         $('#pet_likes').click(function(e){
+           $('#pet_eats').slideDown(200);
+         });
         
+
         $('#myForm').submit(function(e){
             e.preventDefault();//no need to submit as you'll be doing AJAX on this page
             let feels = $("input[name=feels]:checked").val();
             let likes = $("input[name=likes]:checked").val();
             let eats = $("input[name=eats]:checked").val();
-            let pet = "";
-            alert(feels);
+            let pet = "ERROR";
+            let output = "";
 
-
+            if(feels == "fluffy" && likes == "petted" && eats == "carrots"){
+              pet ="rabbit";
+            }
+            output += `<p>Your pet is a ${pet}.</p>`;
+            output += `<p>Your pet feels ${feels}.</p>`;
+            output += `<p>Your pet likes to be ${likes}.</p>`;
+            output += `<p>Your pet likes to eat ${eats}.</p>`;
+            //alert(feels);
+            //output submitted info and replace form
+            $('#output').html(output);
         });
 
     });
